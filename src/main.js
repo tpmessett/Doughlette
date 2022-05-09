9362ad496d22f71bfc53bc639162af895060d1c9
thomas messett <thomas.messett@stampapp.me>
2022-05-09 22:23:38 +0100

    outline setup

--- src/main.js
+++ src/main.js
@@ -1,4 +1,29 @@
 import { createApp } from 'vue'
+import { createStore } from 'vuex'
 import App from './App.vue'
+//import VueApollo from "vue-apollo";
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import { provideApolloClient } from '@vue/apollo-composable'
+import { useQuery } from "@vue/apollo-composable";
+import { gql } from "@apollo/client/core"
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  uri: 'https://partner.api.slerp.com/v1/graphql',
+  headers: {
+  authorization: 'Bearer ' + process.env.VUE_APP_KEY,
+
+    }
+  });
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
 
 createApp(App).mount('#app')
