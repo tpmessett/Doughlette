ed3afb737727759b90b492424b8bb4beb03753c5
thomas messett <thomas.messett@stampapp.me>
2022-05-30 16:48:57 +0100

    added new styles

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
     <img class="logo-image" alt="logo" src="./assets/logo.png">
-  <div id="content">
-    <h1 v-if="result">{{result.store_variants[0].product_variant.name}}</h1>
-    <h3 v-if="result">Date: {{result.store_variants[0].product_variant.product.category.special_availabilities[0].start_date}}</h3>
-    <p v-if="result">{{result.store_variants[0].product_variant.product.description}}</p>
-    <div>
+  <div v-if="result && urlFound == true" id="content">
+    <h1 v-if="result.store_variants.length != 0">{{result.store_variants[0].product_variant.name}}</h1>
+    <h3 v-if="result.store_variants != 0">Date: {{result.store_variants[0].product_variant.product.category.special_availabilities[0].start_date}}</h3>
+    <p v-if="result.store_variants != 0">{{result.store_variants[0].product_variant.product.description}}</p>
+    <div v-if="result.store_variants != 0">
       <div class="tickets">
         <div class="align-items">
           Number of Tickets:
@@ -12,9 +12,10 @@
         <div class="number-of">
           <div @click="numberMinus()" class="align-items"><i class="pi pi-minus-circle"></i></div>
           <div class="number-of align-items">{{number}}</div>
-          <div @click="number++" class="align-items"><i class="pi pi-plus-circle"></i></div>
+          <div @click="maxGuests()" class="align-items"><i class="pi pi-plus-circle"></i></div>
         </div>
       </div>
+      <div class="max-guests" v-if="maxReached"> No more than {{number}} guests </div>
       <div class="input-div">
         <div class="w50 align-center">
           <label for="fname">First name:</label>
@@ -61,31 +62,49 @@
         </div>
       </div>
     </div>
-    <div class="button-container">
+    <div v-if="result && result.store_variants.length > 0" class="button-container">
       <div @click="validate()" class='button'>BOOK NOW</div>
     </div>
   </div>
+  <div v-if="products && urlFound == false">
+    <h1>UPCOMING EVENTS</h1>
+    <div v-for="(product, index) in products.result.value.products" :key="index" style="padding: 8px 16px; margin-bottom: 8px">
+      <h3>{{product.name}}</h3>
+      <h5>{{product.category.special_availabilities[0].start_date}}</h5>
+      <p class="desc">{{product.description}}</p>
+      <a :href="product.sku" class='button'>BOOK NOW</a>
+    </div>
+  </div>
+  <br>
 </template>
 
 <script lang="ts">
 import { ref } from 'vue'
 import { useQuery } from "@vue/apollo-composable";
-import { useMutation } from '@vue/apollo-composable';
+//import { useMutation } from '@vue/apollo-composable';
 import { gql } from "@apollo/client/core"
 
 export default {
   setup(){
+    const urlFound = ref(false)
     const getSku = () => {
       const url = window.location.pathname
+      if (url.length > 1) {
+        urlFound.value = true
+      } else {
+        urlFound.value = false
+      }
       return url.slice(1)
     }
     const number = ref(1)
+    const firstName = ref("")
+    const maxReached = ref(false)
     const numberMinus = () => {
       if (number.value > 1) {
         number.value--
+        maxReached.value = false
       }
     }
-    const firstName = ref("")
     const firstNameEmpty = ref(false)
     const lastName = ref("")
     const lastNameEmpty = ref(false)
@@ -122,7 +141,8 @@ export default {
         invalidGuests.value = false
       }
       if (firstName.value && lastName.value && emailTest(email.value) && phone.value && guestscheck()) {
-        createCheckout()
+        //createCheckout()
+        console.log('done')
       }
     }
     const guestscheck = () => {
@@ -130,6 +150,15 @@ export default {
         return false
       }  else {
         return true
+      }
+    }
+
+    const maxGuests = () => {
+      if(number.value < result.value.store_variants[0].product_variant.product.default_limit) {
+        number.value++
+        maxReached.value = false
+      } else {
+        maxReached.value = true
       }
     }
 
@@ -143,10 +172,10 @@ export default {
       }
     }
     const sku = getSku()
-    const store = process.env.VUE_APP_STORE
+    //const store = process.env.VUE_APP_STORE
     const PRODUCT_QUERY = gql`
       query getBySku {
-        store_variants(where: {product_variant: {sku: {_eq: "${sku}"}}, store: {slug: {_eq: "${store}"}}}) {
+        store_variants(where: {product_variant: {sku: {_eq: "${sku}"}}}) {
           variant_id
           in_stock
           stock_count
@@ -158,6 +187,7 @@ export default {
             price
             product {
               description
+              default_limit
               category {
                 special_availabilities
               }
@@ -166,40 +196,86 @@ export default {
         }
       }
     `
-    const CREATE_CART = gql `
-      mutation CreateSlerpCart($orderItems: [OrderItemParams]) {
-        createSlerpCart(
-          storeId: "",
-          merchantId: "",
-          fulfillmentType: "pickup",
-          address: "56 old street, London, e1",
-          fulfillmentDate: "",
-          fulfillmentTime: "asap",
-          orderItems: $orderItems,
-        ) {
-          id
-        }
-      }
-    `
-    const CREATE_PAYMENT = gql `
-      mutation WebCartPayment($cartId:ID!, $cancelUrl:String!, $successUrl:String!) {
-        payViaWeb(cartId:$cartId, cancelUrl:$cancelUrl, mode: "stripe", successUrl:$successUrl) {
-          sessionData
-        }
-      }
-    `
     const { result, error } = useQuery(PRODUCT_QUERY)
-    const createCheckout = () => {
-      const items = [{
-        productVariantId: result.store_variants[0].variant_id,
-        quantity: number.value,
-        appliedModifiers: []
-      }]
-      const cart = createCart(items)
-    }
+
+    const ALL_PRODUCTS = gql`
+      query MyQuery {
+        products(where: {merchant: {slug: {_eq: "maisonestelle"}}}) {
+          name
+          description
+          category {
+            special_availabilities
+          }
+          sku
+        }
+      }
+    `
+    const products = useQuery(ALL_PRODUCTS)
+    console.log(products)
+    // const GET_VALID_STORE = gql`
+    //   query getValidStore ($date: String!)
+    //          {
+    //           getValidStore(
+    //               storeId: "40694597-494c-4209-ac53-2b87c9a90c81",
+    //               fulfillmentType: "pickup",
+    //               fulfillmentDate: $date,
+    //               address: ""
+    //             )
+    //             {
+    //               name
+    //               id
+    //               fulfillmentTimeslots(fulfillmentDate: $date) {
+    //                 endTime
+    //                 startTime
+    //                 value
+    //             }
+    //           }
+    //         }
+    // `
+
+    // const { mutate: createCart } = useMutation(gql `
+    //   mutation CreateSlerpCart($orderItems: [OrderItemParams], $date: String!) {
+    //     createSlerpCart(
+    //       storeId: "40694597-494c-4209-ac53-2b87c9a90c81",
+    //       merchantId: "${process.env.VUE_APP_MERCHANT_ID}",
+    //       fulfillmentType: "pickup",
+    //       address: "",
+    //       fulfillmentDate: $date,
+    //       fulfillmentTime: "asap",
+    //       orderItems: $orderItems,
+    //     ) {
+    //       id
+    //     }
+    //   }
+    // `)
+
+    // const { mutate: createPayment } = useMutation(gql `
+    //   mutation WebCartPayment($cartId:ID!, $cancelUrl:String!, $successUrl:String!) {
+    //     payViaWeb(cartId:$cartId, cancelUrl:$cancelUrl, mode: "stripe", successUrl:$successUrl) {
+    //       sessionData
+    //     }
+    //   }
+    // `)
+
+    // const createCheckout = () => {
+    //   const date = result.value.store_variants[0].product_variant.product.category.special_availabilities[0].start_date
+    //   const gvs = useQuery(GET_VALID_STORE, () => ({
+    //   date: date,
+    // }))
+    //   console.log(gvs)
+    //   const items = [{
+    //     productVariantId: result.value.store_variants[0].variant_id,
+    //     quantity: number.value,
+    //     appliedModifiers: []
+    //   }]
+    //   const dates = result.value.store_variants[0].product_variant.product.category.special_availabilities[0].start_date
+    //   //const cart = createCart({orderItems: items, date: dates})
+    //   return cart
+    // }
     return {
       sku,
       result,
+      products,
       error,
       number,
       numberMinus,
@@ -213,7 +289,10 @@ export default {
       invalidEmail,
       invalidPhone,
       guests,
-      invalidGuests
+      invalidGuests,
+      maxGuests,
+      maxReached,
+      urlFound
     }
   }
 }
@@ -237,9 +316,13 @@ body {
 .input-div {
   width: 100%;
  /* text-align: left;*/
-  padding-bottom:  16px;
+  padding-top:  16px;
   display:  flex;
   justify-content:  space-around;
+}
+
+a {
+  text-decoration: none;
 }
 
 .small {
@@ -259,13 +342,20 @@ input {
 .tickets {
   display:  flex;
   justify-content:  space-around;
-  padding-bottom:  16px;
 }
 
 .w50 {
   width: 50%;
 }
 
+h3 {
+  font-size: 16px
+}
+
+.desc {
+  padding-bottom: 18px;
+  font-size: 14px;
+}
 .align-center {
   display:  flex;
   align-items: center;
@@ -302,6 +392,12 @@ input {
 h1 {
   font-size: 20px;
   font-weight: 900;
+}
+
+.max-guests {
+  color:red;
+  text-align: right;
+  font-size: 12px;
 }
 
 .button {
